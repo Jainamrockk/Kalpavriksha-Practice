a9753f0eff16fa886c303f58ade4402211de1a4a
Jainam  Jain <jainam.jain@intimetec.com>
2025-01-21 08:50:04 +0530

    Updated Merge Sort

--- assignments-EOD-20-01-25/mergeSort.c
+++ assignments-EOD-20-01-25/mergeSort.c
@@ -1,32 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-typedef struct ListNode{
+typedef struct ListNode
+{
 
     int data;
-    struct ListNode* next;
-}ListNode;
+    struct ListNode *next;
+} ListNode;
 
-ListNode* createList()
+ListNode *createList()
 {
-    ListNode* head = NULL;
-    ListNode* tail = NULL;
-    while(1)
+    ListNode *head = NULL;
+    ListNode *tail = NULL;
+    while (1)
     {
         int value;
-        scanf("%d",&value);
+        scanf("%d", &value);
 
-        if(head == NULL)
+        if (head == NULL)
         {
-            head = (ListNode*)malloc(sizeof(ListNode*));
+            head = (ListNode *)malloc(sizeof(ListNode *));
             head->data = value;
             head->next = NULL;
             tail = head;
         }
         else
         {
-            ListNode* node = (ListNode*)malloc(sizeof(ListNode*));
+            ListNode *node = (ListNode *)malloc(sizeof(ListNode *));
             node->data = value;
             tail->next = node;
             tail = tail->next;
@@ -34,92 +34,78 @@ ListNode* createList()
         }
 
         char ch = getchar();
-        if(ch == '\n' || ch == EOF)
-        break;
+        if (ch == '\n' || ch == EOF)
+            break;
     }
     return head;
 }
 
-void printLinkedList(ListNode* head)
+void printLinkedList(ListNode *head)
 {
-    if(head == NULL)
+    if (head == NULL)
     {
         printf("Empty");
     }
-    while(head)
+    while (head)
     {
-        printf("%d ",head->data);
+        printf("%d ", head->data);
         head = head->next;
     }
-   
+}
+void swap(int *a,int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+ListNode *merge(ListNode *left,ListNode* right)
+{
+
+    if(left == NULL) return right;
+    if(right == NULL) return left;
+
+    if(left->data<=right->data)
+    {
+        left->next = merge(left->next,right);
+        return left;
+    }
+    right->next = merge(left,right->next);
+    return right;
 }
 
-ListNode* merge(ListNode* left , ListNode* mid)
+ListNode *calcMid(ListNode *head)
 {
-    if(left == NULL || left->next == NULL)
-    return left;
-    ListNode* nn = left;
-    while(nn!=mid)
-    {
-        nn = nn->next;
-    }
-    mid = mid->next;
-    nn->next = NULL;
-    nn = left;
-    while(left!=NULL && mid!=NULL)
-    {
-        if(left->data<=mid->data)
-        {
-            left = left->next;
-        }
-        else
-        {
-            ListNode* temp = mid;
-            mid = mid->next;
-            left->next = temp;
-            left = left->next;
-            left->next = NULL;
-        }
-    }
-    
-    left->next = mid;
-
-    return nn;
-}
-
-ListNode* calcMid(ListNode* head)
-{
-    if(head == NULL || head->next == NULL)
+    if (head == NULL || head->next == NULL)
         return head;
-    ListNode* slow = head;
-    ListNode* fast = head->next;
-    while(fast!=NULL && fast->next!=NULL)
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while (fast!=NULL && fast->next != NULL)
     {
         slow = slow->next;
         fast = fast->next->next;
     }
-    return slow;
+    ListNode* temp = slow->next;
+    slow->next = NULL;
+    return temp;
 }
 
-ListNode* mergeSort(ListNode* left)
+ListNode *mergeSort(ListNode *left)
 {
-    if(left == NULL)
-    return NULL;
-    ListNode* mid = calcMid(left);
-    left =  mergeSort(left);
-    mid->next = mergeSort(mid->next);
+    if (left == NULL || left->next == NULL)
+        return left;
+    ListNode* mid = calcMid(left);   
+    left = mergeSort(left);
+    mid = mergeSort(mid);
     left = merge(left,mid);
-    return left;
+    return left; 
 }
-
-
 
 int main()
 {
     printf("Enter the linked list: ");
-    ListNode* head = createList();
+    ListNode *head = createList();
     head = mergeSort(head);
+    printf("Sorted Linked List is: ");
     printLinkedList(head);
     return 0;
 }
-
