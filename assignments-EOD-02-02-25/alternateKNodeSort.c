9e2f00839749b1f45df89655f670824af83bbb2c
Jainam  Jain <jainam.jain@intimetec.com>
2025-02-04 00:16:51 +0530

    Updated Solution

--- assignments-EOD-02-02-25/alternateKNodeSort.c
+++ assignments-EOD-02-02-25/alternateKNodeSort.c
@@ -7,11 +7,10 @@ typedef struct listNode
     struct listNode *next;
 } listNode;
 
-
-int listLength(listNode* head)
+int listLength(listNode *head)
 {
     int length = 0;
-    while(head)
+    while (head)
     {
         length++;
         head = head->next;
@@ -19,131 +18,141 @@ int listLength(listNode* head)
     return length;
 }
 
-
-listNode* createList()
+listNode *createList()
 {
-    listNode* head = NULL;
-    listNode* tail = NULL;
-    while(1)
+    listNode *head = NULL;
+    listNode *tail = NULL;
+    while (1)
     {
         int value;
-        scanf("%d",&value);
-        if(head == NULL)
+        scanf("%d", &value);
+        if (head == NULL)
         {
-            head = (listNode*)malloc(sizeof(listNode));
+            head = (listNode *)malloc(sizeof(listNode));
             head->val = value;
             head->next = NULL;
             tail = head;
             continue;
         }
-        listNode* node = (listNode*)malloc(sizeof(listNode));
+        listNode *node = (listNode *)malloc(sizeof(listNode));
         node->val = value;
         node->next = NULL;
         tail->next = node;
         tail = node;
         char ch = getchar();
-        if(ch == '\n' || ch == EOF)
-        break;
-
+        if (ch == '\n' || ch == EOF)
+            break;
     }
     return head;
 }
 
-void printLinkedList(listNode* head)
+void printLinkedList(listNode *head)
 {
-    while(head)
+    while (head)
     {
-        printf("%d",head->val);
-        if(head->next!=NULL)
-        printf("-> ");
+        printf("%d", head->val);
+        if (head->next != NULL)
+            printf("-> ");
         head = head->next;
     }
 }
 
-listNode* findMid(listNode* left)
+listNode *findMid(listNode *left)
 {
-    if(!left || !left->next)
-    return left;
-    listNode* slow = left;
-    listNode* fast = left;
-    while(!fast && !fast->next)
+    if (!left || !left->next)
+        return left;
+    listNode *slow = left;
+    listNode *fast = left;
+    while (!fast && !fast->next)
     {
         fast = fast->next->next;
-        if(!fast)
-        slow = slow->next;
+        if (!fast)
+            slow = slow->next;
     }
-    listNode* temp = slow->next;
+    listNode *temp = slow->next;
     slow->next = NULL;
     return temp;
 }
 
-
-listNode* merge(listNode* left,listNode* right)
+listNode *merge(listNode *left, listNode *right)
 {
-    if(!left) return right;
-    if(!right) return left;
-    if(left->val<right->val)
+    if (!left)
+        return right;
+    if (!right)
+        return left;
+    if (left->val < right->val)
     {
-        left->next = merge(left->next,right);
-        return left->next;
+        left->next = merge(left->next, right);
+        return left;
     }
-    right->next = merge(left,right->next);
+    right->next = merge(left, right->next);
     return right;
 }
 
-
-listNode* mergeSort(listNode* head)
+listNode *mergeSort(listNode *head)
 {
-    if(head == NULL || head->next == NULL)
-    return head;
+    if (head == NULL || head->next == NULL)
+        return head;
 
-    listNode* mid = findMid(head);
+    listNode *mid = findMid(head);
     head = mergeSort(head);
-    mid = mergeSort(mid->next);
-    head = merge(head,mid);
+    mid = mergeSort(mid);
+    head = merge(head, mid);
     return head;
 }
 
-listNode* applySort(listNode* head,int operationalLength,int k)
+listNode *applySort(listNode *head, int operationalLength, int k)
 {
-    listNode* temp = head;
-    int flag = 0,first = 0;
-    while(operationalLength>0 && temp!=NULL)
+    listNode *temp = head;
+    listNode *prev = NULL;
+    int flag = 0, first = 0;
+    while (operationalLength > 0)
     {
         operationalLength -= k;
         int tempK = k;
-        listNode* left = temp;
-        while(tempK>=1)
+        listNode *left = temp;
+        while (tempK > 1)
         {
             temp = temp->next;
             tempK--;
         }
-        if(!flag%2){
-        listNode* right = temp->next;
-        temp->next = NULL;
-        left = mergeSort(left);
-        if(first == 0)
+        if (flag % 2 == 0)
         {
-            head = left;
-            first = 1;
+            listNode *right = temp->next;
+            temp->next = NULL;
+            left = mergeSort(left);
+            if (first == 0)
+            {
+                head = left;
+                first = 1;
+            }
+            else if (prev != NULL)
+            {
+                prev->next = left;
+            }
+            while (temp->next != NULL)
+                temp = temp->next;
+            temp->next = right;
         }
-        temp->next = right;
+        prev = temp;
+        temp = temp->next;
+        flag = 1 - flag;
     }
-    flag = 1 - flag;
-    }
-    return head;    
+    return head;
 }
 
 int main()
 {
     int k;
     printf("Enter the linked list: ");
-    listNode* head = createList();
+    listNode *head = createList();
     printf("Enter K:");
-    scanf("%d",&k);
+    scanf("%d", &k);
     int Length = listLength(head);
-    int operationalLength =Length - Length%k;
-    head = applySort(head,operationalLength,k);
+    if(k>Length)
+    return 0;
+    int operationalLength = Length - Length % k;
+    head = applySort(head, operationalLength, k);
     printLinkedList(head);
     return 0;
 }
